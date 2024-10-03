0f3ddb9d9342c2ab805a2317b446da6a40492453
88jonhan <88jonhan@skola.boras.se>
2024-10-03 11:12:46 +0200

    styled form

--- screens/FormScreen.tsx
+++ screens/FormScreen.tsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import {
+	View,
+	Text,
+	TextInput,
+	Button,
+	StyleSheet,
+	Pressable,
+} from "react-native";
 import { Controller, useForm } from "react-hook-form";
 import { useData } from "../DataProvider";
 import Slider from "@react-native-community/slider";
@@ -34,24 +41,9 @@ export function FormScreen() {
 	});
 
 	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: "center",
-				alignItems: "center",
-				padding: 15,
-			}}
-		>
-			<View
-				style={{
-					flexDirection: "row",
-					alignItems: "center",
-					justifyContent: "space-between",
-					width: "100%",
-					// gap: 10,
-				}}
-			>
-				<Text style={{ fontSize: 30 }}>Förnamn:</Text>
+		<View style={styles.container}>
+			<View style={styles.inputContainer}>
+				<Text style={styles.inputLabel}>Förnamn:</Text>
 				<Controller
 					control={control}
 					rules={{
@@ -59,7 +51,7 @@ export function FormScreen() {
 					}}
 					render={({ field: { onChange, onBlur, value } }) => (
 						<TextInput
-							style={styles.textInput}
+							style={styles.inputField}
 							onBlur={onBlur}
 							onChangeText={onChange}
 							value={value}
@@ -69,16 +61,8 @@ export function FormScreen() {
 				/>
 				{errors.firstName && <Text>This is required.</Text>}
 			</View>
-			<View
-				style={{
-					flexDirection: "row",
-					alignItems: "center",
-					justifyContent: "space-between",
-					width: "100%",
-					// gap: 10,
-				}}
-			>
-				<Text style={{ fontSize: 30 }}>Efternamn:</Text>
+			<View style={styles.inputContainer}>
+				<Text style={styles.inputLabel}>Efternamn:</Text>
 				<Controller
 					control={control}
 					rules={{
@@ -86,7 +70,7 @@ export function FormScreen() {
 					}}
 					render={({ field: { onChange, onBlur, value } }) => (
 						<TextInput
-							style={styles.textInput}
+							style={styles.inputField}
 							onBlur={onBlur}
 							onChangeText={onChange}
 							value={value}
@@ -96,53 +80,39 @@ export function FormScreen() {
 				/>
 				{errors.lastName && <Text>This is required.</Text>}
 			</View>
-			<View
-				style={{
-					flexDirection: "row",
-					alignItems: "center",
-					justifyContent: "space-between",
-					width: "100%",
-					// gap: 10,
-				}}
-			>
-				<Text style={{ fontSize: 30 }}>Ålder:</Text>
-				<Controller
-					control={control}
-					rules={{
-						required: false,
-					}}
-					render={({ field: { onChange, onBlur } }) => (
-						<Slider
-							style={styles.ageInput}
-							step={1}
-							minimumValue={1}
-							maximumValue={120}
-							minimumTrackTintColor="#FFFFFF"
-							maximumTrackTintColor="#000000"
-							value={inputAge}
-							onValueChange={(value) => {
-								onChange(value);
-								setInputAge(value);
-							}}
-						/>
-					)}
-					name="age"
-				/>
-				<View>
-					<Text>{inputAge}</Text>
+			<View style={styles.inputContainer}>
+				<Text style={styles.inputLabel}>Ålder:</Text>
+				<View style={styles.ageContainer}>
+					<Controller
+						control={control}
+						rules={{
+							required: false,
+						}}
+						render={({ field: { onChange, onBlur } }) => (
+							<Slider
+								style={styles.ageSlider}
+								step={1}
+								minimumValue={1}
+								maximumValue={120}
+								minimumTrackTintColor="#FFFFFF"
+								maximumTrackTintColor="#000000"
+								value={inputAge}
+								onValueChange={(value) => {
+									onChange(value);
+									setInputAge(value);
+								}}
+							/>
+						)}
+						name="age"
+					/>
+					<View style={styles.ageOutputContainer}>
+						<Text style={styles.ageOutput}>{inputAge}</Text>
+					</View>
 				</View>
 			</View>
 
-			<View
-				style={{
-					flexDirection: "row",
-					alignItems: "center",
-					justifyContent: "space-between",
-					width: "100%",
-					// gap: 10,
-				}}
-			>
-				<Text style={{ fontSize: 30 }}>Kön:</Text>
+			<View style={styles.inputContainer}>
+				<Text style={styles.inputLabel}>Kön:</Text>
 				<Controller
 					control={control}
 					rules={{
@@ -150,73 +120,150 @@ export function FormScreen() {
 					}}
 					render={({ field: { onChange, onBlur } }) => (
 						<View>
-							<Checkbox
-								style={styles.checkbox}
-								value={maleIsChecked}
-								onValueChange={(value) => {
-									onChange(value ? "male" : "");
-									setMaleIsChecked(value);
-									setFemaleIsChecked(false);
-									setOtherIsChecked(false);
-								}}
-								color={maleIsChecked ? "#4630EB" : undefined}
-							/>
-							<Checkbox
-								style={styles.checkbox}
-								value={femaleIsChecked}
-								onValueChange={(value) => {
-									onChange(value ? "female" : "");
-									setMaleIsChecked(false);
-									setFemaleIsChecked(value);
-									setOtherIsChecked(false);
-								}}
-								color={femaleIsChecked ? "#4630EB" : undefined}
-							/>
-							<Checkbox
-								style={styles.checkbox}
-								value={otherIsChecked}
-								onValueChange={(value) => {
-									onChange(value ? "other" : "");
-									setMaleIsChecked(false);
-									setFemaleIsChecked(false);
-									setOtherIsChecked(value);
-								}}
-								color={otherIsChecked ? "#4630EB" : undefined}
-							/>
+							<View style={styles.checkboxContainer}>
+								<Text
+									style={
+										maleIsChecked
+											? [styles.checkboxLabel, styles.checkboxLabelChecked]
+											: [styles.checkboxLabel]
+									}
+								>
+									Male
+								</Text>
+								<Checkbox
+									// style={styles.checkbox}
+									value={maleIsChecked}
+									onValueChange={(value) => {
+										onChange(value ? "male" : "");
+										setMaleIsChecked(value);
+										setFemaleIsChecked(false);
+										setOtherIsChecked(false);
+									}}
+									color={maleIsChecked ? "#4630EB" : undefined}
+								/>
+							</View>
+							<View style={styles.checkboxContainer}>
+								<Text
+									style={
+										femaleIsChecked
+											? [styles.checkboxLabel, styles.checkboxLabelChecked]
+											: [styles.checkboxLabel]
+									}
+								>
+									Female
+								</Text>
+								<Checkbox
+									// style={styles.checkbox}
+									value={femaleIsChecked}
+									onValueChange={(value) => {
+										onChange(value ? "female" : "");
+										setMaleIsChecked(false);
+										setFemaleIsChecked(value);
+										setOtherIsChecked(false);
+									}}
+									color={femaleIsChecked ? "#4630EB" : undefined}
+								/>
+							</View>
+							<View style={styles.checkboxContainer}>
+								<Text
+									style={
+										otherIsChecked
+											? [styles.checkboxLabel, styles.checkboxLabelChecked]
+											: [styles.checkboxLabel]
+									}
+								>
+									Other
+								</Text>
+								<Checkbox
+									// style={styles.checkbox}
+									value={otherIsChecked}
+									onValueChange={(value) => {
+										onChange(value ? "other" : "");
+										setMaleIsChecked(false);
+										setFemaleIsChecked(false);
+										setOtherIsChecked(value);
+									}}
+									color={otherIsChecked ? "#4630EB" : undefined}
+								/>
+							</View>
 						</View>
 					)}
 					name="gender"
 				/>
-				<View>
-					<Text>
-						{maleIsChecked
-							? "male"
-							: femaleIsChecked
-							? "female"
-							: otherIsChecked
-							? "other"
-							: "none"}
-					</Text>
-				</View>
 			</View>
 
-			<Button title="Submit" onPress={onSubmit} />
+			<Pressable style={styles.button} onPress={onSubmit}>
+				<Text style={styles.buttonText}>Submit</Text>
+			</Pressable>
 		</View>
 	);
 }
 
-const styles = {
-	textInput: {
-		width: 200,
+const styles = StyleSheet.create({
+	container: {
+		alignItems: "center",
+		width: "100%",
+		height: "100%",
+	},
+	inputContainer: {
+		width: "80%",
+		margin: 5,
+	},
+	inputLabel: {
+		fontSize: 20,
+		fontWeight: "bold",
+	},
+	inputField: {
 		height: 40,
-		borderColor: "gray",
+		fontSize: 20,
 		borderWidth: 1,
+		borderColor: "grey",
 	},
-	ageInput: {
-		width: 200,
+	ageContainer: {
+		width: "100%",
+		margin: 5,
+		flexDirection: "row",
+	},
+	ageSlider: {
+		flex: 1,
 		height: 40,
 	},
-	checkbox: {
-		margin: 8,
+	ageOutputContainer: {
+		alignItems: "center",
+		justifyContent: "center",
 	},
-};
+	ageOutput: {
+		fontSize: 20,
+		width: 40,
+		fontWeight: "bold",
+		textAlign: "center",
+	},
+	checkboxWrapper: {
+		width: "80%",
+		margin: 5,
+	},
+	checkboxContainer: {
+		flexDirection: "row",
+		alignItems: "center",
+	},
+	checkboxLabel: {
+		fontSize: 20,
+		width: 100,
+	},
+	checkboxLabelChecked: {
+		fontWeight: "bold",
+	},
+	button: {
+		position: "absolute",
+		bottom: 0,
+		height: 50,
+		backgroundColor: "#2196f3",
+		width: "100%",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	buttonText: {
+		color: "white",
+		fontSize: 20,
+	},
+});

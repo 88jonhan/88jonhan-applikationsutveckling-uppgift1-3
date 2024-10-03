44756c321c381e529d0af9c728ad1d55f1e25f9b
88jonhan <88jonhan@skola.boras.se>
2024-10-03 10:16:22 +0200

    added screen-files and tab navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import SubmitsScreen from "./SubmitScreen";
+import { FormScreen } from "./FormScreen";
+import RandomImageScreen from "./RandomImageScreen";
+import DataProvider from "./DataProvider";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		// <DataProvider>
+		<NavigationContainer>
+			<Tab.Navigator>
+				<Tab.Screen name="Formulär" component={FormScreen} />
+				<Tab.Screen name="Submits" component={SubmitsScreen} />
+				<Tab.Screen name="Random image" component={RandomImageScreen} />
+			</Tab.Navigator>
+		</NavigationContainer>
+		// </DataProvider>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ FormScreen.tsx
@@ -0,0 +1,5 @@
+import { View } from "react-native";
+
+export function FormScreen() {
+	return <View></View>;
+}

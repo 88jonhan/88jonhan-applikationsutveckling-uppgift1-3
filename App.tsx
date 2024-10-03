dade07faf44164f31bf2f402dabc9e33f0710cb4
88jonhan <88jonhan@skola.boras.se>
2024-10-03 10:42:14 +0200

    added icons to tabs

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { FormScreen } from "./screens/FormScreen";
 import RandomImageScreen from "./RandomImageScreen";
 import DataProvider from "./DataProvider";
 
+import Ionicons from "@expo/vector-icons/Ionicons";
 const Tab = createBottomTabNavigator();
 
 export default function App() {
@@ -14,9 +15,36 @@ export default function App() {
 		<DataProvider>
 			<NavigationContainer>
 				<Tab.Navigator>
-					<Tab.Screen name="Formulär" component={FormScreen} />
-					<Tab.Screen name="Submits" component={SubmitsScreen} />
-					<Tab.Screen name="Random image" component={RandomImageScreen} />
+					<Tab.Screen
+						name="Formulär"
+						component={FormScreen}
+						options={{
+							tabBarLabel: "Formulär",
+							tabBarIcon: ({ color, size }) => (
+								<Ionicons name="add" color={color} size={size} />
+							),
+						}}
+					/>
+					<Tab.Screen
+						name="Submits"
+						component={SubmitsScreen}
+						options={{
+							tabBarLabel: "Submits",
+							tabBarIcon: ({ color, size }) => (
+								<Ionicons name="list" color={color} size={size} />
+							),
+						}}
+					/>
+					<Tab.Screen
+						name="Random image"
+						component={RandomImageScreen}
+						options={{
+							tabBarLabel: "Random Image",
+							tabBarIcon: ({ color, size }) => (
+								<Ionicons name="image" color={color} size={size} />
+							),
+						}}
+					/>
 				</Tab.Navigator>
 			</NavigationContainer>
 		</DataProvider>
